31b3bb8324b26f5985d08f517499a2ea9d2e2d5d
Ryan Measel <ryanmeasel@gmail.com>
2016-05-29 23:46:33 -0700

    Updated file parser and command line parser

--- gpsxml2csv.py
+++ gpsxml2csv.py
@@ -2,34 +2,41 @@
 #
 # Converts gpsxml files to csv
 
-import sys
-import re
 import csv
+import os
+import re
+import sys
 
-# Get filename to parse from command line
+if len(sys.argv) == 1:
+    print "Usage: " + sys.argv[0] + " <GPSXML file>"
+    exit()
+
 filename = sys.argv[1]
 
 # Open the input and output files
-with open(filename, 'r') as inFile, open('parsed.csv', 'w') as outFile:
+with open(filename, 'r') as inFile, open(filename.rsplit(".", 1)[0] + '.csv', 'w') as outFile:
 
     # Create CSV writer and write header
     writer = csv.writer(outFile)
-    writer.writerow(['bssid','time-sec','time-usec','lat','lon','spd','heading','fix','alt'])
+    writer.writerow(['bssid','source','time-sec','time-usec','lat','lon','spd','heading','fix','alt','signal_dbm','noise_dbm'])
 
     # Parse the input
     for line in inFile:
 
         # We are only interested in data lines which all contain "gps-point"
+        # Valid lines have either 9, 10, or 12 matches depending on
+        # whether they contain "source", "sigal_dbm", and "noise_dbm"
         if "gps-point" in line:
 
             # Match everything in double quotes
-            matches = re.findall(r'"(.*?)"', string)
+            matches = re.findall(r'"(.*?)"', line)
 
-            # Valid lines have either 9 or 11 matches
+            # Insert an empty string 'source'
             if len(matches) == 9:
-                wrtier.writerow(matches)
+                matches.insert(1,'')
 
-            if len(matches) == 11:
-                del matches[1]
-                writer.writerow(matches[0:9])
+            # Insert empty strings 'signal_dbm' and 'noise_dbm'
+            if len(matches) == 9 or len(matches) == 10:
+                matches.extend(['',''])
 
+            writer.writerow(matches)
